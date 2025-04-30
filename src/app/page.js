163ef56f8403b3d5382576540d1663998530d74c
Petterson Rodolpho da Cunha <123033379+pettersonrc@users.noreply.github.com>
2025-04-30 17:17:40 -0300

    att

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,61 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
-import styles from "./page.module.css";
-import Topo from "@/components/Topo";
-import SecaoBanner from "@/components/SecaoBanner";
-import SecaoInformacoes from "@/components/SecaoInformacoes";
+import estilos from "./page.module.css";
+import Logo from "../../public/barbearia.png";
+import { FiMoon } from "react-icons/fi";
+import { CiSun } from "react-icons/ci";
 
 export default function Home() {
+    const [ehTemaEscuro, setEhTemaEscuro] = useState(false);
+    const [botaoIcone, setBotaoIcone] = useState(<FiMoon />);
+
+    const alterarTema = () => {
+        setEhTemaEscuro(!ehTemaEscuro);
+
+        if (ehTemaEscuro) {
+            setBotaoIcone(<FiMoon />);
+        } else {
+            setBotaoIcone(<CiSun />);
+        }
+    };
+
     return (
-        <div>
-            <Topo />
-            <SecaoBanner />
-            <SecaoInformacoes />
+        <div
+            className={ehTemaEscuro ? estilos.modo_escuro : estilos.modo_claro}
+        >
+            <header className={estilos.container_topo}>
+                <div className={estilos.container_largura}>
+                    <Image
+                        src={Logo}
+                        className={estilos.logo}
+                        alt="logo"
+                    ></Image>
+                    <button className={estilos.botao} onClick={alterarTema}>
+                        {botaoIcone}
+                    </button>
+                </div>
+            </header>
+            <section className={estilos.banner}></section>
+            <section className={estilos.secao_informacoes}>
+                <div className={estilos.container_texto}>
+                    <h1>Bem-vindo a Barber Shop</h1>
+                    <p className={estilos.paragrafo}>
+                        Nossa barbearia sempre oferece profissionais de
+                        qualidade e estamos prontos para lidar com suas maiores
+                        expectativas.
+                    </p>
+                    <p className={estilos.paragrafo}>
+                        Nossos serviços são dedicados ao seu sucesso pessoal.
+                        Aqui temos uma equipe premiada que demonstrou o talento
+                        de mestres barbeiros em vários concursos de estilo.
+                        Deixe nosso barbeiro ser seu estilista pessoal e você
+                        nunca ficará desapontado.
+                    </p>
+                    <p className={estilos.paragrafo_destaque}>S. Kelly</p>
+                </div>
+            </section>
         </div>
     );
 }
--- src/components/SecaoBanner/index.jsx
+++
@@ -1,9 +0,0 @@
-import estilos from "./SecaoBanner.module.css"
-
-export default function SecaoBanner() {
-    return (
-        <section className={estilos.banner}>
-            
-        </section>
-    )
-}
